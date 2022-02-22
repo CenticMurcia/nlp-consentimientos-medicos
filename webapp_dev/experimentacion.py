21691c126e2a70f3616e6aad57aec247146d142c
Federico Pardo <fpg23feb98@gmail.com>
2022-02-22 17:11:53 +0100

    Widget de subida de ficheros

--- webapp_dev/experimentacion.py
+++ webapp_dev/experimentacion.py
@@ -1,19 +1,47 @@
-import streamlit
+from io import StringIO
+import streamlit as st
 import requests
 
-#Archivo a ser enviado
-files = {'file': open('../data/SP - January - 2021 - Txt files/4111_OR_ES.txt', 'rb')}
-#Parámetros
-params = {'outf': 'tagged', 'format': 'json', 'lang': 'es'}
-#Enviar petición
-url = "http://www.corpus.unam.mx/servicio-freeling/analyze.php"
-r = requests.post(url, files=files, params=params)
-#Convertir de formato json
-obj = r.json()
 
-# Ejemplo, obtener todos los lemas
-# for sentence in obj:
-#     for word in sentence:
-#         print(word)
+def freeling_processing(document = '4111_OR_ES.txt',
+                        directory = '../data/SP - January - 2021 - Txt files',
+                        language = 'es'):
+    #Archivo a ser enviado
+    file = open(f'{directory}/{document}','rb')
+    files = {'file': file}
+    #Parámetros
+    params = {'outf': 'tagged', 'format': 'json', 'lang': language}
+    #Enviar petición
+    url = "http://www.corpus.unam.mx/servicio-freeling/analyze.php"
+    r = requests.post(url, files=files, params=params)
+    #Convertir de formato json
+    obj = r.json()
+
+    # Cerramos el fichero
+    file.close()
+
+    # Devolvemos el diccionario con la info morfológica
+    return obj
+
+    # Ejemplo para mostrar toda la información del procesado de freeling
+    # for sentence in obj:
+    #     for word in sentence:
+    #         print(word)
+#--------------#
+#### WEBAPP ####
+#--------------#
+def example_callback():
+    print('TEST')
+
+
+st.title('Título de muestra')
+
+uploaded_files = st.file_uploader(label= 'Tus ficheros aquí', accept_multiple_files=True, on_change=example_callback)
+
+for uploaded_file in uploaded_files:
+    string_data = uploaded_file.read()
+    st.write('Fichero leido con éxito :sunglasses:')
+
+
 
 
