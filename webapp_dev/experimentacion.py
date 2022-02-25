cf6c7186aa2c7b2295424895bf5d79894a5c6fc5
Federico Pardo <fpg23feb98@gmail.com>
2022-02-25 13:48:47 +0100

    Extracción de métricas básicas

--- webapp_dev/experimentacion.py
+++ webapp_dev/experimentacion.py
@@ -23,6 +23,26 @@ def freeling_processing(document = '4111_OR_ES.txt',
     # for sentence in obj:
     #     for word in sentence:
     #         print(word)
+
+
+
+def extract_metrics(document):
+    st.write(obj[0][0])
+    # - Número de letras en el texto
+    n_chars = sum([len(word['token']) for phrase in obj for word in phrase])
+    st.write(f"{n_chars}")
+    # - Número de sílabas en el texto
+    # Nope
+    # - Número de palabras en el texto (tokens)
+    n_words = len([word for phrase in obj for word in phrase])
+    st.write(f"{n_words}")
+    # - Número de palabras unicas en el texto (Tipos)
+    n_unique_words = len(set([word['token'] for phrase in obj for word in phrase]))
+    st.write(f"{n_unique_words}")
+    # - Número de frases en el texto (oraciones)
+    n_phrases = len([phrase for phrase in obj])
+    st.write(f"{n_phrases}")
+
 #--------------#
 #### WEBAPP ####
 #--------------#
@@ -37,15 +57,15 @@ uploaded_files = st.file_uploader(label= 'Tus ficheros aquí', accept_multiple_f
 for uploaded_file in uploaded_files:
     string_data = uploaded_file.getvalue()
     string_data = string_data.decode('utf-8')
-    
+
     # ESTA LINEA ME HA LLEVADO 3 HORAS PARA SABER QUE FALLABA
     # Los caracteres vienen separados por su codigo individual, de forma
     # que los acentos vienen codificados como a´ en vez de á. UN DRAMA
     string_data = normalize("NFC", string_data)
-    st.write('Fichero leido con éxito :sunglasses:')
-    st.write(string_data)
+    st.success('Fichero leido con éxito :sunglasses:')
+    #st.write(string_data)
     obj = freeling_processing(document=string_data, language='es')
-    st.write(obj[0])
+    extract_metrics(obj)
 
 
 
