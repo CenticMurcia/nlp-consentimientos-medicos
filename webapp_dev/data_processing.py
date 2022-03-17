b9fe116b1d9c60c54ff40d80c4df0b25786081f1
Federico Pardo <fpg23feb98@gmail.com>
2022-03-17 13:10:38 +0100

    Completed morphological features

--- webapp_dev/data_processing.py
+++ webapp_dev/data_processing.py
@@ -32,7 +32,7 @@ def freeling_processing(document='4111_OR_ES.txt',
     return obj
 
 
-@st.cache
+@st.cache(show_spinner=False)
 def extract_metrics(document):
 
     # -Number of chars in text
@@ -62,16 +62,100 @@ def sum_words(index, char, iterator):
                filter(lambda tag: tag[index] == char, iterator))
 
 
+def count_nouns(noun_tags):
+    nouns = {}
+    nouns['noun_counts'] = len(noun_tags)
+    nouns['common_noun_count'] = sum_words(1, 'C', noun_tags)
+    nouns['proper_noun_count'] = sum_words(1, 'P', noun_tags)
+    nouns['person_noun_count'] = sum_words(4, 'S', noun_tags)
+    nouns['location_noun_count'] = sum_words(4, 'G', noun_tags)
+    nouns['organization_noun_count'] = sum_words(4, 'O', noun_tags)
+    nouns['other_noun_count'] = sum_words(4, 'V', noun_tags)
+
+    return nouns
+
+
+def count_adjectives(adjective_tags):
+    adjectives = {}
+    adjectives['adjectives_total'] = len(adjective_tags)
+    adjectives['ordinal_adjectives'] = sum_words(1, 'O', adjective_tags)
+    adjectives['calificative_adjectives'] = sum_words(1, 'Q', adjective_tags)
+    adjectives['possesive_adjectives'] = sum_words(1, 'P', adjective_tags)
+
+    return adjectives
+
+
+def count_conjuctions(conjunction_tags):
+    conjuctions = {}
+    conjuction_total = len(conjunction_tags)
+    coordinative_conjuctions = sum_words(1, 'C', conjunction_tags)
+    subordinates_conjuctions = sum_words(1, 'S', conjunction_tags)
+
+    return conjuctions
+
+
+def count_adverbs(adverb_tags):
+    adverbs = {}
+    adverbs['adverbs_total'] = len(adverb_tags)
+    adverbs['negative_adverbs'] = sum_words(1, 'N', adverb_tags)
+    adverbs['general_adverbs'] = sum_words(1, 'G', adverb_tags)
+
+    return adverbs
+
+
+def count_verbs(verb_tags):
+    verbs = {}
+    # TODO Finish counting of verbs
+    verbs['verbs_total'] = len(verb_tags)
+    verbs['main_verbs'] = sum_words(1, 'M', verb_tags)
+    verbs['auxiliary_verbs'] = sum_words(1, 'A', verb_tags)
+    verbs['semiauxiliary_verbs'] = sum_words(1, 'S', verb_tags)
+    verbs['indicative_verbs'] = sum_words(2, 'I', verb_tags)
+    verbs['subjunctive_verbs'] = sum_words(2, 'S', verb_tags)
+    verbs['imperative_verbs'] = sum_words(2, 'M', verb_tags)
+    verbs['participle_verbs'] = sum_words(2, 'P', verb_tags)
+    verbs['gerund_verbs'] = sum_words(2, 'G', verb_tags)
+    verbs['infinitive_verbs'] = sum_words(2, 'N', verb_tags)
+    verbs['present_verbs'] = sum_words(3, 'P', verb_tags)
+    verbs['imperfect_verbs'] = sum_words(3, 'I', verb_tags)
+    verbs['future_verbs'] = sum_words(3, 'F', verb_tags)
+    verbs['past_verbs'] = sum_words(3, 'S', verb_tags)
+    verbs['conditional_verbs'] = sum_words(3, 'c', verb_tags)
+
+    return verbs
+
+
+def count_determiners(determiner_tags):
+    determiners = {}
+    determiners['determiner_total'] = len(determiner_tags)
+    determiners['articles_determiners'] = sum_words(1, 'A', determiner_tags)
+    determiners['demostrative_determiners'] = sum_words(
+        1, 'D', determiner_tags)
+    determiners['undefined_determiners'] = sum_words(1, 'I', determiner_tags)
+    determiners['possesive_determiners'] = sum_words(1, 'P', determiner_tags)
+    determiners['interrogative_determiners'] = sum_words(
+        1, 'T', determiner_tags)
+    determiners['exclamative_determiners'] = sum_words(1, 'E', determiner_tags)
+
+    return determiners
+
+
+def count_pronouns(pronoun_tags):
+    pronouns = {}
+    pronouns['pronouns_total'] = len(pronoun_tags)
+    pronouns['fp_pronouns'] = sum_words(2, '1', pronoun_tags)
+    pronouns['sp_pronouns'] = sum_words(2, '2', pronoun_tags)
+    pronouns['tp_pronouns'] = sum_words(2, '3', pronoun_tags)
+
+    return pronouns
+
+
 def morphological_metrics(document):
     """Extracts the number of nouns, verbs, adjectives, etc"""
 
     # Get all tags
-    morpho_count = {}
     i1 = perf_counter()
-    elements = []
-    for phrase in document:
-        elements += phrase
-
+    elements = [word for sentence in document for word in sentence]
     tags = list(map(lambda word: word['tag'], elements))
 
     # Filter by categories
@@ -84,52 +168,32 @@ def morphological_metrics(document):
     pronoun_tags = list(filter(lambda tag: tag[0] == 'P', tags))
 
     # Nouns
-    nouns_count = len(noun_tags)
-    common_noun_count = sum_words(1, 'C', noun_tags)
-    proper_noun_count = sum_words(1, 'P', noun_tags)
-    person_noun_count = sum_words(4, 'S', noun_tags)
-    location_noun_count = sum_words(4, 'G', noun_tags)
-    organization_noun_count = sum_words(4, 'O', noun_tags)
-    other_noun_count = sum_words(4, 'V', noun_tags)
+    nouns = count_nouns(noun_tags)
 
     # Adjectives
-    adjectives_total = len(adjective_tags)
-    ordinal_adjectives = sum_words(1, 'O', adjective_tags)
-    calificative_adjectives = sum_words(1, 'Q', adjective_tags)
-    possesive_adjectives = sum_words(1, 'P', adjective_tags)
+    adjectives = count_adjectives(adjective_tags)
 
     # Conjuctions
-    conjuction_total = len(conjunction_tags)
-    coordinative_conjuctions = sum_words(1, 'C', conjunction_tags)
-    subordinates_conjuctions = sum_words(1, 'S', conjunction_tags)
+    conjuctions = count_conjuctions(conjunction_tags)
 
     # Adverbs
-    adverbs_total = len(adverb_tags)
-    negative_adverbs = sum_words(1, 'N', adverb_tags)
-    general_adverbs = sum_words(1, 'G', adverb_tags)
+    adverbs = count_adverbs(adverb_tags)
 
     # Verbs
-    verbs_total = len(verb_tags)
-    negative_verbs = sum_words(1, 'N', verb_tags)
-    general_verbs = sum_words(1, 'G', verb_tags)
+    verbs = count_verbs(verb_tags)
 
     # Determiner
-    determiner_total = len(determiner_tags)
-    articles_determiners = sum_words(1, 'A', determiner_tags)
-    demostrative_determiners = sum_words(1, 'D', determiner_tags)
-    undefined_determiners = sum_words(1, 'I', determiner_tags)
-    possesive_determiners = sum_words(1, 'P', determiner_tags)
-    interrogative_determiners = sum_words(1, 'T', determiner_tags)
-    exclamative_determiners = sum_words(1, 'E', determiner_tags)
+    determiners = count_determiners(determiner_tags)
 
     # Pronouns
-    pronouns_total = len(pronoun_tags)
-    fp_pronouns = sum_words(2, '1', pronoun_tags)
-    sp_pronouns = sum_words(2, '2', pronoun_tags)
-    th_pronouns = sum_words(2, '3', pronoun_tags)
+    pronouns = count_pronouns(pronoun_tags)
+
+    morpho_count = (nouns | adjectives | conjuctions |
+                    adverbs | verbs | determiners | pronouns)
 
     i2 = perf_counter()
     print(f"Tiempo de procesamiento morfologico: {i2-i1:0.6f}")
+    st.write(morpho_count)
     return morpho_count
 
 
