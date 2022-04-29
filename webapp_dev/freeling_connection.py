67d5f4a2679cd2436d09b61d6a071fbdb475cddc
Federico Pardo <fpg23feb98@gmail.com>
2022-04-29 22:23:18 +0200

    Middle point async request

--- webapp_dev/freeling_connection.py
+++ webapp_dev/freeling_connection.py
@@ -1,11 +1,13 @@
+import asyncio
+import json
 import logging
 import time
 
-import grequests
+import aiohttp
 import requests
 import streamlit as st
 
-from data_processing import transform_language, read_file
+from data_processing import read_file
 
 
 @st.experimental_memo(show_spinner=False)
@@ -34,39 +36,68 @@ def freeling_processing(files, language):
             raise UnicodeError(f'''File **{uploaded_file.name}** is not
                     encoded in UTF-8 or Latin-1''')
 
-        request = create_freeling_request(document=string_data,
-                                          filename=uploaded_file.name,
-                                          language=language,
-                                          async_req=True)
         strings.append(string_data)
         names.append(uploaded_file.name)
-        requests_list.append(request)
 
+    morphological_analysis = asyncio.run(freeling_requests(strings, names,
+                                                           language))
     # Collection containing an object for every file the
     # morphological_analysis. Transforming it to a json...
     with st.spinner('Procesando ficheros en freeling...'):
         time1 = time.perf_counter()
-        morphological_analysis = grequests.imap(requests_list, size=10,
-                                                exception_handler=handler)
+
         morphological_jsons = [clean_json(element.json()) for element
                                in morphological_analysis]
     time2 = time.perf_counter()
-    logging.info(time2-time1)
+    logging.info(time2 - time1)
     return zip(morphological_jsons, strings, names)
 
 
-def handler(request, exception):
-    print(exception)
-    print(request)
+def get_tasks(session, files, language):
+    tasks = []
+    url = 'http://frodo.lsi.upc.edu:8080/TextWS/textservlet/ws/processQuery/morpho'
+    for document, filename in files:
+        data = aiohttp.FormData()
+
+        # data.add_field('username',st.secrets['api_username'])
+        # data.add_field('password', st.secrets['api_passwd'])
+        # data.add_field('filename', filename)
+        # data.add_field('text_input', document)
+        # data.add_field('language', language)
+        # data.add_field('output', 'json')
+        # data.add_field('interactive', 1)
+        payload = {'username': st.secrets['api_username'],
+                   'password': st.secrets['api_passwd'],
+                   'filename': filename,
+                   'text_input': document,
+                   'language': language,
+                   'output': 'json',
+                   'interactive': '1'}
+
+        data.add_field('files', json.dumps(payload))
+
+        tasks.append(session.post(url, json=payload))
+    return tasks
+
+
+async def freeling_requests(strings, filenames, language='es'):
+    logging.info(f"Cache miss -> create_freeling_requests()")
+    async with aiohttp.ClientSession() as session:
+        tasks = get_tasks(session, zip(strings, filenames), language)
+        responses = await asyncio.gather(*tasks)
+    return responses
+
+
+def clean_json(json_file):
+    return [sentence['tokens'] for paragraph in json_file['paragraphs'] for
+            sentence in paragraph['sentences']]
 
 
 def connect_server():
     retries = 0
     server_up = False
     while not server_up and retries < 5:
-        request = create_freeling_request(
-            document='Test',
-            language='es')
+        request = check_server()
         if not request:
             retries += 1
             time.sleep(20)
@@ -80,27 +111,14 @@ def connect_server():
             f'los ficheros. Inténtelo de nuevo más tarde.')
 
 
-def create_freeling_request(document, filename='4088.txt', language='es',
-                            async_req=False):
-    logging.info(f"Cache miss -> create_freeling_requests()")
-
-    language = transform_language(language)
+def check_server():
     request_data = {'username': st.secrets['api_username'],
                     'password': st.secrets['api_passwd'],
-                    'filename': filename,
-                    'text_input': document,
-                    'language': language,
+                    'filename': 'File',
+                    'text_input': 'Test',
+                    'language': 'es',
                     'output': 'json',
                     'interactive': '1'}
-
     url = 'http://frodo.lsi.upc.edu:8080/TextWS/textservlet/ws/processQuery/morpho'
-    if async_req:
-        r = grequests.post(url, files=request_data)
-    else:
-        r = requests.post(url, files=request_data)
-    return r
-
-
-def clean_json(json_file):
-    return [sentence['tokens'] for paragraph in json_file['paragraphs'] for
-            sentence in paragraph['sentences']]
+    resp = requests.post(url, files=request_data)
+    return resp
